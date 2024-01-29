dd37fae37cca1b21248fa17cc1029c7d100bc58d
fathimathjazeera <jezijazeera@gmail.com>
2024-01-29 14:20:27 +0530

    registration,validation

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import "dotenv/config"
 import mongoose from 'mongoose'
+import userRoutes  from './routes/users'
 
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -10,9 +11,9 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
 
+app.use("/api/users", userRoutes)
 
 
-app.get('/api/test')
 
 
 app.listen(7000,()=>{
